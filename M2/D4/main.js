0310293ef43a3958a5bfc1c5064a217d1668df5b
Giorgio-g97 <124620661+Giorgio-g97@users.noreply.github.com>
2023-06-02 10:40:13 +0200

    Aggiunti file teoria + Es. aggiustato, però da fare

--- M2/D4/main.js
+++ M2/D4/main.js
@@ -8,7 +8,13 @@ assicurati di COMPRENDERE la logica. Se ti trovi in difficolta', scrivi ad un me
 
 Se l'utente ha la proprietà "isAmbassador" con valore true, il carrello deve venire scontato del 30%, PRIMA di calcolare la spedizione (anche gli utenti speciali pagano le spedizioni).
 Se l'utente ha la proprietà "isAmbassador" con valore false, il carrello NON deve venire scontato.
-In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito per coprire il costo della spedizione. */
+In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito per coprire il costo della spedizione.
+
+In basso troverai degli esempi di utenti, una lista prezzi e un costo fisso di spedizione.
+Crea un array di utenti (usando .push) e stampa, per ogni utente (quindi con un loop) la frase "NOMEUTENTE COGNOMEUTENTE e' / non e' un ambassador" basandoti sui dati contenuti nell'oggetto. 
+ES. L'utente Marco Rossi e' un ambassador, quindi la frase dovrebbe essere "Marco Rossi e' un ambassador"
+Infine, crea un SECONDO array in cui inserirai SOLO gli ambassador.
+*/
 
 const marco = {
   name: "Marco",
---
+++ M2/D4/teoria.md
@@ -0,0 +1,2 @@
+# D4 - Array
+Struttura dati che permette di accedere a valori multipli tramite unico nome.
