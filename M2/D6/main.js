4cf66fd83040a5deb1e750e8a4350bbc37d23ceb
Giorgio-g97 <124620661+Giorgio-g97@users.noreply.github.com>
2023-06-13 22:05:50 +0200

    Iniziato D6 a metà, da finire

--- M2/D6/main.js
+++ M2/D6/main.js
@@ -1,9 +1,49 @@
 // getElementById
 const container = document.getElementById('container')
-console.log('container')
+// console.log(container)
 // getElementByClassName
-const description = document.getElementByClassName('description')
-console.log('description')
+const description = document.getElementsByClassName('description')
+// console.log(description)
 // getElementByTagName
-const images = document.getElementByTagName('img')
-console.log('images')
+const images = document.getElementsByTagName('img')
+// console.log(images)
+
+// Modifico immagini con un ciclo for of, dato che mi ha restituito un array di oggetti (immagini)
+for (const i of images) {
+    i.style.width = '300px'
+    i.style.height = '200px'
+}
+
+// Queryselector (class)
+const featuredClass = document.querySelector('.featured') // in questo caso è la stessa cosa di getelementbyclassname
+// console.log(featuredClass)
+
+// Queryselector (id)
+const containerId = document.querySelector('#container') // in questo caso è la stessa cosa di getelementbyid
+// console.log(containerId)
+
+// querySelector con più classi/id
+const featuredMul = document.querySelector('.featured .bg-image')
+// console.log(featuredMul)
+
+// QueryselectorAll
+const featuredAll = document.querySelectorAll('.featured')
+// console.log(featuredAll)
+
+
+// leggere contetnuto elemento id (es. contenuto testo)
+const titolo = document.getElementById('titolo')
+// console.log(titolo.innerHTML)
+
+// // leggere contetnuto elemento classi (es. contenuto testo)
+const par = document.getElementsByClassName('paragrafo')
+// console.log(par[0].innerHTML) // serve indicare l'indice dell'array
+
+// modificare testo
+// titolo.innerText = 'Provaaa'
+// par[0].innerText = 'prova11'
+
+// Es. bottone 
+const heading = getElementById('myHead')
+const headClass = getElementsByClassName('prova')
+heading[0].classList.add('red-color')
--- M2/D6/teoria.md
+++ M2/D6/teoria.md
@@ -9,9 +9,7 @@ Come interagire:
 * **document.getElementById**: seleziona singolo oggetto (dato che nell'HTML dovrebbe esserci solo un elemento avente ID univoco)
 * **document.getElementByClassName**: seleziona **array** di oggetti aventi la stessa classe
 * **document.getElementByTagName**: **array** di oggetti avente stessa classe
-* **document.querySelector**: selezione **selettore specifico CSS**
-* **document.querySelectorAll**: seleziona tutti i **selettori CSS**
+* **document.querySelector**: selezione **selettore specifico di classe o id**
+* **document.querySelectorAll**: seleziona tutti i **selettori di classe o id** (per gli id non ha senso)
 * **node.children**: seleziona array di elementi figli
-## Esempi
-```
-```
+## Esempi in main.js
