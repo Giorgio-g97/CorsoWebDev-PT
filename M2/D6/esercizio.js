8e766d3f3a2c34fc7553a6dea4584441ee9dd945
Giorgio-g97 <124620661+Giorgio-g97@users.noreply.github.com>
2023-06-14 17:54:37 +0200

    Fatti tutti gli esercizi D6

--- M2/D6/esercizio.js
+++ M2/D6/esercizio.js
@@ -13,8 +13,42 @@ chColor()
 
 // Es. 3 JS
 function chAdd(){
-const add = document.getElementsByTagName('address')
-add.innerHTML = 'Corso Matteotti 3'
-console.log(add)
+const indir = document.getElementsByTagName('address')
+indir[0].innerHTML = 'Corso Matteotti 3'
 }
 chAdd()
+
+// Es. 4 JS
+function linkC(){
+    const linkA = document.querySelectorAll('a')
+    for (i=0; i<=linkA.length; i++){
+    linkA[i].classList.add('link-color')
+    }
+}
+linkC()
+
+// Es. 5 JS
+function remImg(){
+  const rmImg = document.querySelectorAll('img')
+  for (i=0;i<=rmImg.length;i++){
+    rmImg[i].classList.toggle('img-hidden')
+  }
+}
+
+// Es. 6 JS
+function getRanCol(){
+    const red = Math.round(Math.random() * 255)
+      const green = Math.round(Math.random() * 255)
+      const blue = Math.round(Math.random() * 255)
+
+      return 'rgb(' + red + ', ' + green + ', ' + blue + ')'
+}
+const rC = getRanCol()
+console.log(rC)
+function ranCol(){
+    const rCol = getRanCol()
+    const prices = document.querySelectorAll('.price')
+    for(i=0;i<=prices.length;i++){
+        prices[i].style.color = rCol 
+    }
+}
